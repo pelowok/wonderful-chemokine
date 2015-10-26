af85a6251977b1251ae5df273fdb881b1cfb6b7c
pelowok <zak@mynameiszak.com>
2015-10-25 21:41:16 -0400

    fullscreen videogular running, catching ngTouchEvent

--- app/scripts/modules/module_myApp.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by zp on 10/25/2015.
- */
-
-var app = angular.module('appInfi', ['ngTouch', 'ngAnimate'])
-    .controller('mainController', function($scope){
-        console.log("mainController fired");
-    })
