a75b4886317d255e835fa8396d31aea263d0d6a6
pelowok <zak@mynameiszak.com>
2015-11-15 01:56:22 -0500

    ch1 fully operational, building ch2. subnav side menu complete

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -119,12 +119,15 @@ arrControllers.SubNavController = function($scope, $state, $sce) {
     console.log('SubNavController assigned');
 
     $scope.subnavstate = "closenav";
+    $scope.boolSideNavOpen = false;
 
     $scope.flipSubNav = function(){
         if($scope.subnavstate === "closenav"){
             $scope.subnavstate = "opennav";
+            $scope.boolSideNavOpen = true;
         } else {
             $scope.subnavstate = "closenav";
+            $scope.boolSideNavOpen = false;
         }
     }
 
@@ -258,6 +261,8 @@ arrControllers.Z1Controller  = function($scope, $state, $sce) {
 
 arrControllers.Q1Controller = function ($scope, $interval, $state, $sce){
 
+    console.log('Q1Controller fired');
+
     $scope.btnImg = [
         { id: 'red', btn: 'q1b1', img: '/images/red.png', class: 'q1b' },
         { id: 'red', btn: 'q1b2', img: '/images/red.png', class: 'q1b' },
@@ -370,10 +375,6 @@ arrControllers.Q1Controller = function ($scope, $interval, $state, $sce){
         msgReps++;
     }, 2000, 4);
 
-
-
-
-
 }
 
 arrControllers.S1Controller = function ($scope, $interval, $state, $sce) {
@@ -472,87 +473,6 @@ arrControllers.V1Controller = function ($scope, $interval, $state, $sce) {
         },
         autoPlay: true
     };
-}
-
-arrControllers.DEFUNCT_QUIZCONTROLLER = function ($scope,$http,$sce) {
-
-    console.log('$scope.partThisIs from inside QuizController : ' + $scope.partThisIs);
-    $scope.score = 0;
-    $scope.activeQuestion = -1;
-    $scope.activeQuestionAnswered = 0;
-    $scope.percentage = 0;
-
-    $http.get('../quiz_data.json').then( function(quizData) {
-        // $http.get('http://mynameiszak.com/sandbox/quiz/quiz_data.json').then( function(quizData) {
-
-        console.log('QuizController fired');
-
-        $scope.myQuestions = quizData.data;
-        $scope.totalQuestions = $scope.myQuestions.length;
-
-    });
-
-    $scope.selectAnswer = function ( qIndex, aIndex ) {
-
-        var questionState = $scope.myQuestions[qIndex].questionState;
-
-        if( questionState != 'answered'){
-
-            $scope.myQuestions[qIndex].selectedAnswer = aIndex;
-
-            var correctAnswer = $scope.myQuestions[qIndex].correct;
-            $scope.myQuestions[qIndex].correctAnswer = correctAnswer;
-
-            if( aIndex === correctAnswer ){
-
-                $scope.myQuestions[qIndex].correctness = 'correct';
-                $scope.score += 1;
-
-            } else {
-
-                $scope.myQuestions[qIndex].correctness = 'incorrect';
-
-            }
-
-            $scope.myQuestions[qIndex].questionState = 'answered';
-
-        }
-
-        $scope.percentage = (( $scope.score / $scope.totalQuestions ) * 100 ).toFixed(0) ;
-
-    }
-
-    $scope.isSelected = function( qIndex, aIndex ){
-
-        return $scope.myQuestions[qIndex].selectedAnswer === aIndex;
-
-    }
-
-    $scope.isCorrect = function( qIndex, aIndex ){
-
-        return $scope.myQuestions[qIndex].correctAnswer === aIndex;
-
-    }
-
-    $scope.selectContinue = function(){
-        return $scope.activeQuestion += 1;
-    }
-
-    $scope.createShareLinks = function(percentage){
-
-        var url = 'http://zipl.org/sandbox/quiz';
-
-        var emailLink = '<a class="btn email" href="mailto:?subject=Potato&amp;body=I scored a ' + percentage + '% on this quiz at ' + url + '">Email a friend</a>';
-
-        var twitterLink = '<a class="btn twitter" target="_blank" href="#">Tweet your score</a>';
-
-        var newMarkup = emailLink + twitterLink;
-
-        return $sce.trustAsHtml(newMarkup);
-
-
-    }
-
 }
 
 arrControllers.FullVideoController = function($sce, $scope, $interval, $state) {
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,128 +2,104 @@
  * Created by zp on 10/26/2015.
  */
 
-var infiApp = angular.module('infiApp',
-    [
-        'ngSanitize',
-        'ngTouch',
-        'ngAnimate',
-        'ui.router',
-        'com.2fdevs.videogular',
-        'com.2fdevs.videogular.plugins.controls',
-        'com.2fdevs.videogular.plugins.overlayplay',
-        'com.2fdevs.videogular.plugins.poster',
-        'com.2fdevs.videogular.plugins.buffering'
-    ]
-);
+'use strict';
+
+var infiApp = angular.module('infiApp', ['ngSanitize', 'ngTouch', 'ngAnimate', 'ui.router', 'com.2fdevs.videogular', 'com.2fdevs.videogular.plugins.controls', 'com.2fdevs.videogular.plugins.overlayplay', 'com.2fdevs.videogular.plugins.poster', 'com.2fdevs.videogular.plugins.buffering']);
 
 infiApp.config(function ($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('/attract');
 
-    $stateProvider
-        .state('mainmenu', {
-            url: '/mainmenu',
-            views: {
-                'main': {
-                    templateUrl: '/partials/mainmenu.html',
-                    controller: 'MainMenuController'
-                }
+    $stateProvider.state('mainmenu', {
+        url: '/mainmenu',
+        views: {
+            'main': {
+                templateUrl: '/partials/mainmenu.html',
+                controller: 'MainMenuController'
             }
-        })
-
-        .state('attract', {
-            url: '/attract',
-            views: {
-                'main': {
-                    templateUrl: '/partials/fullvideo.html',
-                    controller: 'AttractController'
-                }
+        }
+    }).state('attract', {
+        url: '/attract',
+        views: {
+            'main': {
+                templateUrl: '/partials/fullvideo.html',
+                controller: 'AttractController'
             }
-        })
-
-        .state('c1', {
-            url: '/c1',
-            views: {
-                'main': {
-                    templateUrl: '/partials/c1.html'
-                },
-                'layer1@c1': {
-                    templateUrl: '/partials/vid1.html',
-                    controller: 'C1Controller'
-                }
+        }
+    }).state('c1', {
+        url: '/c1',
+        views: {
+            'main': {
+                templateUrl: '/partials/ch.html'
+            },
+            'layer1@c1': {
+                templateUrl: '/partials/vid.html',
+                controller: 'C1Controller'
             }
-        })
-
-        .state('c1.tranz', {
-            url: '/tranz',
-            views: {
-                'layer2@c1': {
-                    templateUrl: 'partials/vid1.html',
-                    controller: 'Z1Controller'
-                }
+        }
+    }).state('c1.tranz', {
+        url: '/tranz',
+        views: {
+            'layer1@c1': {
+                template: ''
+            },
+            'layer2@c1': {
+                templateUrl: 'partials/vid.html',
+                controller: 'Z1Controller'
             }
-        })
-
-        .state('c1.quiz', {
-            url: '/quiz',
-            views: {
-                'layer2@c1': {
-                    templateUrl: 'partials/q1.html',
-                    controller: 'Q1Controller'
-                },
-                'layer3@c1': {
-                    templateUrl: '/partials/subnav.html',
-                    controller: 'SubNavController'
-                }
-            }
-        })
-
-        .state('c1.snapshot', {
-            url: '/snapshot',
-            views: {
-                'layer2@c1': {
-                    templateUrl: 'partials/s1.html',
-                    controller: 'S1Controller'
-                }
+        }
+    }).state('c1.quiz', {
+        url: '/quiz',
+        views: {
+            'layer2@c1': {
+                templateUrl: 'partials/q1.html',
+                controller: 'Q1Controller'
             },
             'layer3@c1': {
                 templateUrl: '/partials/subnav.html',
                 controller: 'SubNavController'
             }
-        })
-
-        .state('c1.teaser', {
-            url: '/teaser',
-            views: {
-                'layer2@c1': {
-                    templateUrl: 'partials/t1.html',
-                    controller: 'T1Controller'
-                }
+        }
+    }).state('c1.snapshot', {
+        url: '/snapshot',
+        views: {
+            'layer2@c1': {
+                templateUrl: 'partials/s1.html',
+                controller: 'S1Controller'
             },
             'layer3@c1': {
                 templateUrl: '/partials/subnav.html',
                 controller: 'SubNavController'
             }
-        })
-
-        .state('c1.video', {
-            url: '/video',
-            views: {
-                'layer2@c1': {
-                    templateUrl: 'partials/v1.html',
-                    controller: 'V1Controller'
-                }
+        }
+    }).state('c1.teaser', {
+        url: '/teaser',
+        views: {
+            'layer2@c1': {
+                templateUrl: 'partials/t1.html',
+                controller: 'T1Controller'
+            },
+            'layer3@c1': {
+                templateUrl: '/partials/subnav.html',
+                controller: 'SubNavController'
             }
-        })
-
-        .state('fullvideo', {
-            url: '/fullvideo',
-            views: {
-                'main': {
-                    templateUrl: '/partials/fullvideo.html',
-                    controller: 'FullVideoController'
-                }
+        }
+    }).state('c1.video', {
+        url: '/video',
+        views: {
+            'layer2@c1': {
+                templateUrl: 'partials/v1.html',
+                controller: 'V1Controller'
             }
-        });
+        }
+    }).state('fullvideo', {
+        url: '/fullvideo',
+        views: {
+            'main': {
+                templateUrl: '/partials/fullvideo.html',
+                controller: 'FullVideoController'
+            }
+        }
+    });
 });
 
